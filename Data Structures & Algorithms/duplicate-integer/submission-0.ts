cd5e7efc36fd1e08e5021ccec38f229cbbe05ac9
Yeabsra Mesfin <73798018+boompow@users.noreply.github.com>
2026-09-25 10:27:29 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const set:Set<number> = new Set()
+
+        for(let num of nums){
+            if(set.has(num)){
+                return true
+            }else{
+                set.add(num)
+            }
+        }
+
+        return false
+    }
+}
